5c2890fd740745cd4fe06d5f81858524e1695834
Richard <76814730+MARTINrichard05@users.noreply.github.com>
2023-12-07 09:16:42 +0100

    Tp4 exo4 part 1

--- TP4/MostFrequent.py
+++ TP4/MostFrequent.py
@@ -3,11 +3,21 @@ L1 = [2, 7, 5, 6, 7, 1, 6, 2, 1, 7, 6]
 """ ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** *
 * Completez le programme a partir d'ici.
 ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** * /
-
-
-
-
-
+"""
+#sortie : Le nombre le plus frequent dans la liste est le : 7 (3 x)
+workingdict= {}
+for i in L1:
+    if i in workingdict:
+        workingdict[i]+=1
+    else:
+        workingdict[i]=1
+max=0
+maxkey=0
+for i in workingdict:
+    if workingdict[i]>max:
+        max=workingdict[i]
+        maxkey=i
+print("Le nombre le plus frequent dans la liste est le :",maxkey,"(",max,"x)")
 
 
 
